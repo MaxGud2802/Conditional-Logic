8ffbb5c15b92687cfe8ea8776b05a4413e758f42
Maximus Gudino <m_gudino@swbell.net>
2021-12-20 18:38:13 -0600

    this is what i have so far for the conditional logic exercise for 2.8

--- Conditional Logic/ViewController.swift
+++ Conditional Logic/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageTopScreen: UILabel!
+    @IBOutlet weak var imageViewerFirst: UIImageView!
+    @IBOutlet weak var backgroundScreen: UIView!
+    @IBOutlet weak var labelBackgroundColor: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageTopScreen.text = "I wonder what this button does"
+        messageTopScreen.textAlignment = .center
+        messageTopScreen.textColor = .black
+        backgroundScreen.backgroundColor = .clear
+        labelBackgroundColor.backgroundColor = .white
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if messageTopScreen.text == "Wow! Try pushing it another time." {
+            messageTopScreen.text = "Push the button again!"
+            messageTopScreen.textAlignment = .center
+            messageTopScreen.textColor = .white
+            backgroundScreen.backgroundColor = .clear
+            labelBackgroundColor.backgroundColor = .purple
+            imageViewerFirst.image = UIImage(named: "image2")
+        } else {
+            messageTopScreen.text = "Wow! Try pushing it another time."
+            messageTopScreen.textAlignment = .center
+            messageTopScreen.textColor = .black
+            backgroundScreen.backgroundColor = .clear
+            labelBackgroundColor.backgroundColor = .yellow
+            imageViewerFirst.image = UIImage(named: "image3")
+        }
+
     }
 
-
 }
-
